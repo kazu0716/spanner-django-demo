d9afce00be097bf4ac466a80adcbae8405bac96b
kazu0716 <kazu2055.tennis@gmail.com>
2022-03-31 22:28:07 +0900

    add: model-viewset in user and scores

--- app/scores/views.py
+++ app/scores/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from models import Score
+from serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = Score.objects.all()
+    serializer = UserSerializer(queryset, many=True)
